75b6a304feec6113409621d4698a3e90eb2ed40d
Luther Tchofo Safo <nintendobserver@gmail.com>
2026-05-05 21:03:50 +0200

    TSDoc copiée-collée à la main.

--- declarations/source/library/utilities/index.d.ts
+++ declarations/source/library/utilities/index.d.ts
@@ -1,16 +1,36 @@
-export function makeSuccessFalseTypeError<T extends string, U extends string>(message: T, status: U): {
-    readonly success: false;
-    readonly errors: readonly [{
-        readonly type: "error";
-        readonly message: T;
-        readonly status: U;
-    }];
+/**
+ * $COMMENT#JSDOC#DEFS#MAKESUCCESSFALSETYPEERROR
+ * @param message $COMMENT#JSDOC#PARAMS#MESSAGE
+ * @param status $COMMENT#JSDOC#PARAMS#STATUS
+ */
+export function makeSuccessFalseTypeError<T extends string, U extends string>(
+  message: T,
+  status: U,
+): {
+  readonly success: false;
+  readonly errors: readonly [
+    {
+      readonly type: "error";
+      readonly message: T;
+      readonly status: U;
+    },
+  ];
 };
-export function makeSuccessFalseTypeWarning<T extends string, U extends string>(message: T, status: U): {
-    readonly success: false;
-    readonly errors: readonly [{
-        readonly type: "warning";
-        readonly message: T;
-        readonly status: U;
-    }];
+/**
+ * $COMMENT#JSDOC#DEFS#MAKESUCCESSFALSETYPEWARNING
+ * @param message $COMMENT#JSDOC#PARAMS#MESSAGE
+ * @param status $COMMENT#JSDOC#PARAMS#STATUS
+ */
+export function makeSuccessFalseTypeWarning<T extends string, U extends string>(
+  message: T,
+  status: U,
+): {
+  readonly success: false;
+  readonly errors: readonly [
+    {
+      readonly type: "warning";
+      readonly message: T;
+      readonly status: U;
+    },
+  ];
 };
