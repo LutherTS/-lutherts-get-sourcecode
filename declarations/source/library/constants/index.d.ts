43c7ff5ace370f050e3e51579b79af6fa05310bb
Luther Tchofo Safo <nintendobserver@gmail.com>
2026-05-05 19:12:17 +0200

    New is utilities.

--- declarations/source/library/constants/index.d.ts
+++ declarations/source/library/constants/index.d.ts
@@ -1,16 +1,16 @@
-/** Apply last with `...successFalse` to ensure that `success: false` sits at the top of its object's type definition, like `{propertyA, propertyB, ...successFalse}`. */
+/** $COMMENT#JSDOC#CONSTS#SUCCESSFALSE */
 export const successFalse: Readonly<{
     success: false;
 }>;
-/** Apply last with `...successTrue` to ensure that `success: true` sits at the top of its object's type definition, like `{propertyA, propertyB, ...successTrue}`. */
+/** $COMMENT#JSDOC#CONSTS#SUCCESSTRUE */
 export const successTrue: Readonly<{
     success: true;
 }>;
-/** Apply last with `...typeError` to ensure that `type: "error"` sits at the top of its object's type definition, like `{propertyA, propertyB, ...typeError}`. */
+/** $COMMENT#JSDOC#CONSTS#TYPEERROR */
 export const typeError: Readonly<{
     type: "error";
 }>;
-/** Apply last with `...typeWarning` to ensure that `type: "warning"` sits at the top of its object's type definition, like `{propertyA, propertyB, ...typeWarning}`. */
+/** $COMMENT#JSDOC#CONSTS#TYPEWARNING */
 export const typeWarning: Readonly<{
     type: "warning";
 }>;
